8577d09f5609f2fe0ad6e4254751e010d93829a5
Matej Pangerc <matej@idoru.ai>
2024-05-07 11:28:45 +0200

    favicon and title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  buildModules: ["@nuxt/image"],
+  app: {
+    head: {
+      title: "Casa Glicine Milano",
+      link: [
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/img/favicon/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/img/favicon/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/img/favicon/favicon-16x16.png",
+        },
+        { rel: "manifest", href: "/img/favicon/site.webmanifest" },
+      ],
+    },
+  },
   css: ["~/assets/css/main.css"],
 
   postcss: {
